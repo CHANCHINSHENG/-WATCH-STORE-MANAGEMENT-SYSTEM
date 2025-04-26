b5789d31ba34a5effa6a1c82cc9fca70db964642
TEELIJOHN <1221208013@soffice.mmu.edu.my>
2025-04-26 22:21:01 +0800

    new changes

--- New_Fyp/fyp/admin_delete_product.php
+++ New_Fyp/fyp/admin_delete_product.php
@@ -1,6 +1,7 @@
 <?php
-session_start();
-include 'db.php';
+require_once 'admin_login_include/db.php';
+require_once 'admin_login_include/config_session.php';
+
 
 if (!isset($_SESSION['admin_id'])) {
     header("Location: admin_login.php");
@@ -12,10 +13,10 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $product_id = $_GET['id'];
 
     // Delete the product
-    $stmt = $conn->prepare("DELETE FROM 05_PRODUCT WHERE ProductID = ?");
-    $stmt->bind_param("i", $product_id);
+    $stmt = $pdo->prepare("DELETE FROM 05_PRODUCT WHERE ProductID = ?");
+    $stmt->bindparam("i", $product_id);
 
-    if ($stmt->execute()) {
+    if ($stmt->execute([$product_id])) {
         echo "✅ Product deleted successfully!";
     } else {
         echo "❌ Error deleting product.";
