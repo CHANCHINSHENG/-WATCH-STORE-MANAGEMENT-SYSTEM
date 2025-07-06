2028fc89d73e3b4b4886636bf418b492bcdf0ab0
TEELIJOHN <1221208013@soffice.mmu.edu.my>
2025-07-06 21:25:21 +0800

    commit

--- admin_check_new_orders.php
+++ admin_check_new_orders.php
@@ -2,12 +2,15 @@
 require_once 'admin_login_include/config_session.php';
 require_once 'admin_login_include/db.php';
 
-$stmt = $pdo->prepare("SELECT o.OrderID, o.Total_Price, o.OrderDate AS OrderDateTime,
-                              c.Cust_Username, c.CustomerID
-                       FROM 08_order o
-                       JOIN 02_customer c ON o.CustomerID = c.CustomerID
-                       WHERE o.OrderStatus = 'Processing' AND o.Admin_Payment_Confirmation = 'Pending'
-                       ORDER BY o.OrderDate DESC");
+$stmt = $pdo->prepare("
+    SELECT o.OrderID, o.Total_Price, o.OrderDate AS OrderDateTime,
+           c.Cust_Username, c.CustomerID
+    FROM 08_order o
+    JOIN 02_customer c ON o.CustomerID = c.CustomerID
+    WHERE o.OrderStatus = 'Processing' AND o.Admin_Payment_Confirmation = 'Pending'
+    ORDER BY o.OrderDate DESC, o.OrderID DESC
+");
+
 
 $stmt->execute();
 $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
